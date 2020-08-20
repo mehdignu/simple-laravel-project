a5974edc982bb21b6831e5a8ae2668d6d164bd86
mehdi <dridignu@gmail.com>
2020-08-20 22:04:13 +0200

    add migrations

---
+++ database/migrations/2020_08_20_195745_create_residents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResidentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('residents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('firstname');
+            $table->string('surname');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('residents');
+    }
+}
